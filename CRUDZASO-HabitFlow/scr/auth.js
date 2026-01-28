323f2c540a5edd19d01397b172061e1b22c64926
Santiago Villa Panesso <santiagovillapanesso1@gmail.com>
2026-01-27 20:32:30 -0500

    feat:structure in mind and images were added

---
+++ CRUDZASO-HabitFlow/scr/auth.js
@@ -0,0 +1,24 @@
+const userEmail = document.getElementById('email')
+const userPass = document.getElementById('pass')
+const userButton = document.getElementById('button')
+
+
+userButton.addEventListener('click', function(){
+
+const emailWrite = inputEmail.value;
+const passWrite = inputPassword.value;
+
+
+let encontrado = false
+
+
+
+
+
+
+
+
+
+}
+
+
